dd092c9e064c41468f22cc179b258183a04d76e5
caillatlucas <caillatlucas2304@gmail.com>
2026-06-11 12:50:46 +0200

    Update RoundResult.tsx

--- src/components/RoundResult.tsx
+++ src/components/RoundResult.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { MapContainer, TileLayer, Marker, Polyline } from 'react-leaflet';
 import L from 'leaflet';
 import type { LocationData } from '../utils/gameData';
